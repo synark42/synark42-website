d1d0bc6c9a2ddefd5eb5e28f18bea937bc200cc6
Synark42 <synark42.innovations@gmail.com>
2025-10-07 10:37:37 +0530

    Add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,13 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Synark42",
   description: "Created with v0",
   generator: "v0.app",
-  icons: ['/logo.svg'],
+  icons: ["/logo.svg"],
 };
 
 export default function RootLayout({
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
